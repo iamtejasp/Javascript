5833f8b7bc30246eb105daa24149cc859d91a7c4
Tejas Patel <tejas7016652830@gmail.com>
2024-09-05 09:46:31 +0530

    update: code added

---
+++ web_dev_js/05_Fetch.js
@@ -0,0 +1,6 @@
+// window.fetch();
+// console.log(fetch("https://jsonplaceholder.typicode.com/users")); return promise
+
+fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+  console.log(res)
+);
