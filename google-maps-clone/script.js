b2a171f9e6da2f85ad41694dfa1ff33b49f76941
Tejas Patel <pateltejas5112@gmail.com>
2025-09-06 11:21:51 +0530

    add mapbox setup in js

---
+++ google-maps-clone/script.js
@@ -0,0 +1,38 @@
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1Ijoid2ViZGV2c2ltcGxpZmllZCIsImEiOiJja2dyYTRqbW0weWl1MnJxaWF2dGR0ZHMwIn0.lU-OINCILi52P5N98qMbtA";
+
+//pk.eyJ1Ijoid2FzaGluZ3RvbjI5OSIsImEiOiJja2dzaTNkMjEwNDM0MzFvZnNnNTNxNjNvIn0.c0RymMFd_Q9NADrTGZh7wg
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+// navigator?.geolocation?.watchPosition(successLocation, errorLocation, {
+//   enableHighAccuracy: true,
+// });
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: centerPosition,
+    zoom: 17,
+  });
+
+  const navigationControls = new mapboxgl.NavigationControl();
+  map.addControl(navigationControls);
+
+  const directionControls = new MapboxDirections({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+  });
+  map.addControl(directionControls, "top-left");
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
